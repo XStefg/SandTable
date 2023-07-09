f8e6bb4207f0a1d48fda55f1361c04f6123212f9
EDDYFINDT\SGuerin <sguerin@eddyfi.com>
2023-07-09 12:07:09 -0400

    Add intermediate position ForwardKinetic

--- SandTableEngine.Tests/ForwardKineticTests.cs
+++ SandTableEngine.Tests/ForwardKineticTests.cs
@@ -7,7 +7,7 @@ public class ForwardKineticTests
 {
   [TestMethod]
   [DynamicData( nameof(AdditionData) )]
-  public void TestMethod1( Angle angle1, Angle angle2, Distance xPosition, Distance yPosition )
+  public void TestMethod1( Angle angle1, Angle angle2, Distance x1Position, Distance y1Position, Distance x2Position, Distance y2Position )
   {
     // Arrange
     KineticParameters parameters = CreateParameters();
@@ -15,21 +15,27 @@ public class ForwardKineticTests
     // Act
     ForwardKinetic kinetic = ForwardKinetic.ComputeFor( parameters, angle1, angle2 );
 
-    // Aert
-    kinetic.Y.Value.Should().BeApproximately( yPosition, 0.001 );
-    kinetic.X.Value.Should().BeApproximately( xPosition, 0.001 );
+    // Assert
+    kinetic.X1.Value.Should().BeApproximately( x1Position, 0.001 );
+    kinetic.Y1.Value.Should().BeApproximately( y1Position, 0.001 );
+    kinetic.X2.Value.Should().BeApproximately( x2Position, 0.001 );
+    kinetic.Y2.Value.Should().BeApproximately( y2Position, 0.001 );
   }
 
   public static IEnumerable<object[]> AdditionData =>
-    new[] { 
-            new object[] { Angle.CreateFromDegree( 180.0 ),  Angle.CreateFromDegree( 90.0 ), Distance.CreateFromMeter( -2.0 ), Distance.CreateFromMeter( -1.0 ) },
-            new object[] { Angle.CreateFromDegree( 90.0 ),  Angle.CreateFromDegree( -90.0 ), Distance.CreateFromMeter( 1.0 ), Distance.CreateFromMeter( 2.0 ) },
-            new object[] { Angle.CreateFromDegree( -90.0 ),  Angle.CreateFromDegree( 90.0 ), Distance.CreateFromMeter( 1.0 ), Distance.CreateFromMeter( -2.0 ) },
-            new object[] { Angle.CreateFromDegree( -90.0 ),  Angle.CreateFromDegree( 0.0 ), Distance.CreateFromMeter( 0.0 ), Distance.CreateFromMeter( -3.0 ) },
-            new object[] { Angle.CreateFromDegree(  0.0  ),  Angle.CreateFromDegree( 0.0 ), Distance.CreateFromMeter( 3.0 ), Distance.CreateFromMeter( 0.0 ) },
-            new object[] { Angle.CreateFromDegree(  90.0 ),  Angle.CreateFromDegree( 0.0 ), Distance.CreateFromMeter( 0.0 ), Distance.CreateFromMeter( 3.0 ) },
-            new object[] { Angle.CreateFromDegree(  0.0  ),  Angle.CreateFromDegree( 90.0 ), Distance.CreateFromMeter( 2.0 ), Distance.CreateFromMeter( 1.0 ) },
-          };
+    new[]
+    {
+      Create(   0.0,   0.0,  2.0,  0.0,  3.0,  0.0 ), 
+      Create(  90.0,   0.0,  0.0,  2.0,  0.0,  3.0 ), 
+      Create(   0.0,  90.0,  2.0,  0.0,  2.0,  1.0 ),
+      Create( 180.0,  90.0, -2.0,  0.0, -2.0, -1.0 ), 
+      Create(  90.0, -90.0,  0.0,  2.0,  1.0,  2.0 ), 
+      Create( -90.0,  90.0,  0.0, -2.0,  1.0, -2.0 ), 
+      Create( -90.0,   0.0,  0.0, -2.0,  0.0, -3.0 ),
+    };
 
   KineticParameters CreateParameters() => new KineticParameters( 2.0, 1.0 );
+
+  private static object[] Create( double angle1, double angle2, Distance x1Position, Distance y1Position, Distance x2Position, Distance y2Position )
+    => new object[] { Angle.CreateFromDegree( angle1 ), Angle.CreateFromDegree( angle2 ), x1Position, y1Position, x2Position, y2Position };
 }
--- SandTableEngine/ForwardKinetic.cs
+++ SandTableEngine/ForwardKinetic.cs
@@ -8,19 +8,25 @@ namespace SandTableEngine;
 
 public class ForwardKinetic
 {
-  private ForwardKinetic( Distance x, Distance y )
+  private ForwardKinetic( Distance x1, Distance y1, Distance x2, Distance y2 )
   {
-    X = x;
-    Y = y;
+    X1 = x1;
+    Y1 = y1;
+    X2 = x2;
+    Y2 = y2;
   }
 
-  public Distance X { get; }
-  public Distance Y { get; }
+  public Distance X1 { get; }
+  public Distance Y1 { get; }
+  public Distance X2 { get; }
+  public Distance Y2 { get; }
 
   public static ForwardKinetic ComputeFor( KineticParameters param, Angle rotation1, Angle rotation2 )
   {
-    Distance x = param.Distance1 * Math.Cos( rotation1 ) + param.Distance2 * Math.Cos( rotation1 + rotation2 );
-    Distance y = param.Distance1 * Math.Sin( rotation1 ) + param.Distance2 * Math.Sin( rotation1 + rotation2 );
-    return new ForwardKinetic( x, y );
+    Distance x1 = param.Distance1 * Math.Cos( rotation1 );
+    Distance y1 = param.Distance1 * Math.Sin( rotation1 );
+    Distance x2 = x1 + param.Distance2 * Math.Cos( rotation1 + rotation2 );
+    Distance y2 = y1 + param.Distance2 * Math.Sin( rotation1 + rotation2 );
+    return new ForwardKinetic( x1, y1, x2, y2 );
   }
 }
